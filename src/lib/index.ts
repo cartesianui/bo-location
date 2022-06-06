f0e6782718274feb6e3066900ee4a53971c21cce
Syed Ali Murtaza <ali@kazmi.me>
2022-06-06 12:30:56 +0500

    Adds index file with lib, to manage top level exports in public-api.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ng-location
+ */
+export * from './ng-location.module';
+export * from './models';
+export * from './location.sandbox';
--- src/public-api.ts
+++ src/public-api.ts
@@ -1,6 +1,4 @@
 /*
  * Public API Surface of ng-location
  */
-export * from './lib/ng-location.module';
-export * from './lib/models';
-export * from './lib/location.sandbox';
+export * from './lib';
